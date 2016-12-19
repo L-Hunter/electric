54061d8f44813a02fcdb0c9788c1d3c6c32e29ba
Mia Adjei <adjei.m@gmail.com>
2016-12-18 19:51:38 -0500

    Add javascript that adds promo code message to contact form when button is clicked.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,4 +14,8 @@ $(document).ready(function() {
 
   setTimeout(show_modal, 2000);
 
+  $('#promocode').click(function(){
+  	$('#promo-message').append("<p>Promo code <span class='emphasis upsize'>SOLSTICE</span> added!</p>")
+  })
+
 });
